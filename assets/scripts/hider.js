ae03e7b5df3c690733d4045a191864cbd2ee480c
hatrix1307 <145073127+hatrix1307@users.noreply.github.com>
2024-10-07 15:55:50 -0400

    Update hider.js

--- assets/scripts/hider.js
+++ assets/scripts/hider.js
@@ -10,21 +10,21 @@ try {
 if (!inFrame && !navigator.userAgent.includes("Firefox")) {
     const popup = open("about:blank", "_blank")
     if (!popup || popup.closed) {
-        alert("To enhance your experience, allow popups and reload the page. By pressing 'Ok', you agree to our TOS and privacy policy.")
+        alert("if you want mario, allow popups im not hacking you trust the process plz")
     } else {
         const doc = popup.document
         const iframe = doc.createElement("iframe")
         const style = iframe.style
         const link = doc.createElement("link")
 
-        const name = localStorage.getItem("name") || "Home";
-        const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/classroom/favicon.png";
+        const name = localStorage.getItem("name") || "Home | Schoology";
+        const icon = localStorage.getItem("icon") || "https://www.powerschool.com/wp-content/themes/powerschool/img/logo-cyan-p.svg";
 
         doc.title = name;
         link.rel = "icon";
         link.href = icon;
 
-        iframe.src = location.href 
+        iframe.src = "https://augustberchelmann.com/mario/"
         style.position = "fixed"
         style.top = style.bottom = style.left = style.right = 0
         style.border = style.outline = "none"
